d9d9403df64db63e6d7e2fbf5c9677c22df174d3
Kazuo256 <kazuo256@gmail.com>
2015-02-09 20:19:44 -0200

    SquareGridElement uses a GridDescriptor

--- lib/ufo/gui/grid/SquareGridElement.lua
+++ lib/ufo/gui/grid/SquareGridElement.lua
@@ -5,14 +5,20 @@ local vec2  = require 'lux.geom.Vector'
 local gui   = class.package 'ufo.gui'
 local grid  = class.package 'ufo.gui.grid'
 
-function grid:SquareGridElement (tile_size, columns, rows, _name)
+function grid:SquareGridElement (tile_size, _name)
   
-  gui.Element:inherit(self, _name, vec2:new{},
-                      vec2:new{columns*tile_size, rows*tile_size})
+  gui.Element:inherit(self, _name, vec2:new{}, vec2:new{1024, 768})
+
+  local grid_descriptor
+
+  function self:setGridDescriptor (descriptor)
+    grid_descriptor = descriptor
+  end
 
   function self:draw (graphics)
-    for i=1,rows do
-      for j=1,columns do
+    if grid_descriptor then
+      for pos, tile, stack in grid_descriptor:eachPosition() do
+        local i, j = pos.i, pos.j
         local t = (i + j) % 2
         graphics.setColor(100, 100 + t*50, 100 + t*50, 255)
         graphics.rectangle('fill', (j-1)*tile_size, (i-1)*tile_size,
