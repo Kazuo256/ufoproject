c708e210e86bbb963bf6c0e79098d2939be8aacb
Kazuo256 <kazuo256@gmail.com>
2015-03-31 14:08:40 -0300

    BatchElement orders sprite using z value

--- lib/ufo/gui/sprite/BatchElement.lua
+++ lib/ufo/gui/sprite/BatchElement.lua
@@ -8,13 +8,26 @@ function sprite:BatchElement (_name)
 
   local sprites = {}
 
+  local function comp (lhs, rhs)
+    return sprites[lhs].z < sprites[rhs].z
+  end
+
   function self:putSprite (pos, sprite)
     sprites[sprite] = pos
   end
 
+  function self:removeSprite (sprite)
+    sprites[sprite] = nil
+  end
+
   function self:draw (graphics)
+    local order = {}
     for sprite,pos in pairs(sprites) do
-      sprite:draw(graphics, pos)
+      table.insert(order, sprite)
+    end
+    table.sort(order, comp)
+    for _,sprite in ipairs(order) do
+      sprite:draw(graphics, sprites[sprite])
     end
   end
 
