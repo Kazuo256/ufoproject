641c1e9b081206185ad8d9bf97a2fc83f69a893f
Kazuo256 <kazuo256@gmail.com>
2015-08-15 21:11:24 -0300

    Migrating class definitions... [4]

--- lib/ufo/gui/grid/SquareGridElement.lua
+++ lib/ufo/gui/grid/SquareGridElement.lua
@@ -22,16 +22,16 @@ function SquareGridElement:instance (obj, _name, tile_num, tile_size, atlas)
   end
 
   local function isTileVisible (i, j)
-    return self:intersects(self:getPos() + tile_size*vec2:new{j-.5, i-.5})
+    return obj:intersects(obj:getPos() + tile_size*vec2:new{j-.5, i-.5})
   end
 
-  function self:set (i, j, ...)
+  function obj:set (i, j, ...)
     local index = toIndex(i,j)
     assert(index >= 1 and index <= tile_num*tile_num)
     tiles[index].content = { ... }
   end
 
-  function self:draw (graphics)
+  function obj:draw (graphics)
     for _,tile in ipairs(tiles) do
       local i, j = tile.i, tile.j
       local pos = tile_size*vec2:new{j-.5, i-.5}
